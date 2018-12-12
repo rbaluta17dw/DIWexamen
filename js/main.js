54b06c41f9310bed6e4e4d75555e6e040a01085f
RobonicBaluta <robert.n.baluta@hotmail.es>
2018-12-12 19:13:33 +0100

    no se

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Constructor Libro
+function Libro(titulo, autor, isbn, generos) {
+  this.titulo = titulo;
+  this.autor = autor;
+  this.isbn = isbn;
+  this.generos = generos;
+}
+
+window.onload = function()
+{
+
+
+  var snd = document.getElementById("snd");
+  snd.addEventListener("click", function() {
+    var  = ;
+
+
+
+  });
+
+
+}
